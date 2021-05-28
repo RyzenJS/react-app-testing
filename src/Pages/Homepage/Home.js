0ccefda34490aa6a8a8a7e0e5cffe9563db932c7
Paul J <pljvlln@gmail.com>
2021-05-29 00:20:08 +0800

    min

--- src/Pages/Homepage/Home.js
+++ src/Pages/Homepage/Home.js
@@ -9,12 +9,9 @@ function Home() {
   return(
     <>
     <div className='Homepage-body'>
-      <nav>
-        <Navbar />
-      </nav>
-
       <header className='moving-banner'>
         <div className='video-container'>
+          <Navbar />
           <video src={Leafy} autoPlay muted loop></video>
         </div>
         <div className='moving-banner-content'>
@@ -24,12 +21,24 @@ function Home() {
         </div>
       </header>
 
-      <div id='about' className='content'>
+      <div id='about' className='content-container'>
         <h1>About</h1>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <blockquote>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </blockquote>
+
       </div>
 
-
+      <footer className='footer-container'>
+        <div className='footer-content'>
+          <h2>Let's talk. Say Hello</h2>
+          <a target='_blank' rel='noreferrer noopener nofollow'><h1>gmail@gmail.com</h1></a>
+        </div>
+      </footer>
     </div>
     </>
   )
