76075e82bcc70e0ebce6528b67d745d8f313ce64
Paul J <pljvlln@gmail.com>
2021-05-29 00:45:34 +0800

    Grid antd

--- src/Pages/Homepage/Home.js
+++ src/Pages/Homepage/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {Button} from 'antd';
+import {Button, Row, Col, Divider} from 'antd';
 import {Link} from 'react-router-dom';
 import Leafy from './Leafy.mp4';
 import Navbar from '../Navbar';
 import './Home.css';
+
+const style = { background: '#446B40', padding: '8px 0' };
 
 function Home() {
   return(
@@ -17,12 +19,11 @@ function Home() {
         <div className='moving-banner-content'>
           <h1>Vegetables</h1>
           <h3>Full screen video landing page</h3>
-          <a href='#about'><Button className='banner-btn'>Read More</Button></a>
+          <a href='#product'><Button className='banner-btn'>Read More</Button></a>
         </div>
       </header>
 
-      <div id='about' className='content-container'>
-        <h1>About</h1>
+      <div id='product' className='content-container'>
         <blockquote>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit,
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -31,11 +32,30 @@ function Home() {
           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </blockquote>
 
+        <div className='product-container'>
+
+
+          <Divider orientation="left"><h1>All Products</h1></Divider>
+          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            <Col className="gutter-row" span={6}>
+              <div style={style}>col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div style={style}>col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div style={style}>col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div style={style}>col-6</div>
+            </Col>
+          </Row>
+        </div>
       </div>
 
       <footer className='footer-container'>
         <div className='footer-content'>
-          <h2>Let's talk. Say Hello</h2>
+          <h2>Contact Us. Say Hello</h2>
           <a target='_blank' rel='noreferrer noopener nofollow'><h1>gmail@gmail.com</h1></a>
         </div>
       </footer>
