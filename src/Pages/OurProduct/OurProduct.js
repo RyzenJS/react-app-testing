2a0848e9003b4e9cb084e5d4ff52233c01c39e42
Paul J <pljvlln@gmail.com>
2021-06-01 05:30:36 +0800

    product page, fill

--- src/Pages/OurProduct/OurProduct.js
+++ src/Pages/OurProduct/OurProduct.js
@@ -15,6 +15,7 @@ import {
 import {ProductData} from './Product/ProductData';
 import '../Homepage/Home.css';
 import './OurProduct.css';
+import Leafy from '../Homepage/Banner/Leafy.mp4';
 
 const { Title, Paragraph } = Typography;
 const { Meta } = Card;
@@ -24,40 +25,44 @@ function OurProduct() {
   return (
     <AppContainer>
       <div className='OurProduct-body'>
-        <header className='Picture-banner'>
-          <img src='../Homepage/Banner/Leafy.jpg' />
-          <Title level={2}>All Products</Title>
+        <header className='product-moving-banner'>
+          <div className='product-video-container'>
+            <video src={Leafy} autoPlay muted loop></video>
+          </div>
+          <div className='product-moving-banner-content'>
+            <Title level={2}>All Products</Title>
+          </div>
         </header>
 
         <div className='product-container'>
-        <Divider orientation="left"><h2>Vegetables</h2></Divider>
-        <Row>
-        {
-          ProductData.map((item, index) => {
-            return(
-              <Col>
-              <Link to={item.path}>
-              <Card
-              hoverable
-              style={{ width: 240}}
-              cover={item.image}
-              >
-              <Meta title={item.title} description={item.description} />
-              </Card>
-              </Link>
-              </Col>
-            )
-          })
-        }
-        </Row>
+          <Divider orientation="left"><h2>Vegetables</h2></Divider>
+          <Row>
+            {
+              ProductData.map((item, index) => {
+                return(
+                  <Col>
+                  <Link to={item.path}>
+                  <Card
+                  hoverable
+                  style={{ width: 240}}
+                  cover={item.image}
+                  >
+                  <Meta title={item.title} description={item.description} />
+                  </Card>
+                  </Link>
+                  </Col>
+                )
+              })
+            }
+          </Row>
 
-        <Pagination
-        className='pagination'
-        current='1'
-        simple defaultCurrent={2}
-        total={20}
-        responsive={true}
-        />
+          <Pagination
+          className='pagination'
+          current='1'
+          simple defaultCurrent={2}
+          total={20}
+          responsive={true}
+          />
         </div>
       </div>
     </AppContainer>
