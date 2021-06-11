4810ba5c5503e3867bcca11e6a8852fe24b31593
Paul J <pljvlln@gmail.com>
2021-06-12 02:32:15 +0800

    product data

---
+++ src/utils/products.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    name: "Green Bell Pepper",
+    type: "vegetable",
+    price: 10
+  },
+  {
+    id: 2,
+    name: "Carrot",
+    type: "vegetable",
+    price: 15
+  },
+  {
+    id: 3,
+    name: "Orange Pumpkin",
+    type: "vegetable",
+    price: 20
+  },
+  {
+    id: 4,
+    name: "Tomato",
+    type: "vegetable",
+    price: 10
+  },
+];
